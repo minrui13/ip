d3aac7a23ddf1cab0adb3e20af30ec44778a0962
min rui <gohminrui@gmail.com>
2026-08-17 16:28:30 +0800

    change AGENTS.md and rename bot name

---
+++ src/main/java/Yappa.java
@@ -0,0 +1,11 @@
+public class Yappa {
+    public static void main(String[] args) {
+        String banner = "__   __                    \n"
+                + "\\ \\ / /_ _ _ __  _ __  __ _ \n"
+                + " \\ V / _` | '_ \\| '_ \\/ _` |\n"
+                + "  | | (_| | |_) | |_) | (_| |\n"
+                + "  |_|\\__,_| .__/| .__/ \\__,_|\n"
+                + "          |_|   |_|          \n";
+        System.out.println(banner);
+    }
+}
