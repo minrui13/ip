ba9c05dc2be38d95decb94deb6f23109f714a82e
min rui <gohminrui@gmail.com>
2026-08-17 17:39:37 +0800

    Level 1 - echo

--- src/main/java/Yappa.java
+++ src/main/java/Yappa.java
@@ -1,23 +1,75 @@
+import java.time.LocalTime;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Yappa {
+
+    public static final String LINE = "____________________________________________________________";
+    public static final String LOGO = "__   __                    \n"
+            + "\\ \\ / /_ _ _ __  _ __  __ _ \n"
+            + " \\ V / _` | '_ \\| '_ \\/ _` |\n"
+            + "  | | (_| | |_) | |_) | (_| |\n"
+            + "  |_|\\__,_| .__/| .__/ \\__,_|\n"
+            + "          |_|   |_|          \n";
+
     public static void main(String[] args) {
-        String logo = "__   __                    \n"
-                + "\\ \\ / /_ _ _ __  _ __  __ _ \n"
-                + " \\ V / _` | '_ \\| '_ \\/ _` |\n"
-                + "  | | (_| | |_) | |_) | (_| |\n"
-                + "  |_|\\__,_| .__/| .__/ \\__,_|\n"
-                + "          |_|   |_|          \n";
+        printGreeting();
+        inputAndEcho();
+        printExit();
+    }
 
-        String line = "____________________________________________________________";
-
-        // Greet user
-        System.out.println(line);
-        System.out.println(logo);
-        System.out.println("Hello! I'm Yappa. Ready to yap and get stuff done!");
+    private static void printGreeting() {
+        System.out.println(LINE);
+        System.out.println(LOGO);
+        System.out.println("Good " + getTimeOfDay() + "! I'm Yappa. Ready to yap and get stuff done!");
         System.out.println("What are we tackling today? Let's do this!");
-        System.out.println(line);
+        System.out.println(LINE);
+    }
 
-        // Exit message
-        System.out.println("Catch you later :)!");
-        System.out.println(line);
+    private static void printExit() {
+        printMessage("Catch you later :)!");
+    }
+
+    private static void printMessage(String text) {
+        System.out.println(LINE);
+        System.out.println(text);
+        System.out.println(LINE);
+    }
+
+    public static String getTimeOfDay() {
+        LocalTime currentTime = LocalTime.now();
+        int currentHour = currentTime.getHour();
+
+        String period;
+
+        if (currentHour >= 12 && currentHour < 17) {
+            period = "Afternoon";
+        } else if (currentHour >= 17 && currentHour < 21) {
+            period = "Evening";
+        } else {
+            period = "Morning";
+        }
+
+        return period;
+    }
+
+    public static void inputAndEcho() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String input;
+            while ((input = br.readLine()) != null) {
+                input = input.trim();
+
+                if (input.equalsIgnoreCase("bye")) {
+                    break;
+                }
+
+                System.out.println(LINE);
+                System.out.println("\t" + input);
+                System.out.println(LINE);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading input: " + e.getMessage());
+        }
     }
 }
