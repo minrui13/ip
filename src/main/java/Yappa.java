3bd2861b874e0f88caf86db8e086d448c22369eb
min rui <gohminrui@gmail.com>
2026-08-17 16:53:38 +0800

    update initial code to simply greet the user and exit

--- src/main/java/Yappa.java
+++ src/main/java/Yappa.java
@@ -1,11 +1,23 @@
 public class Yappa {
     public static void main(String[] args) {
-        String banner = "__   __                    \n"
+        String logo = "__   __                    \n"
                 + "\\ \\ / /_ _ _ __  _ __  __ _ \n"
                 + " \\ V / _` | '_ \\| '_ \\/ _` |\n"
                 + "  | | (_| | |_) | |_) | (_| |\n"
                 + "  |_|\\__,_| .__/| .__/ \\__,_|\n"
                 + "          |_|   |_|          \n";
-        System.out.println(banner);
+
+        String line = "____________________________________________________________";
+
+        // Greet user
+        System.out.println(line);
+        System.out.println(logo);
+        System.out.println("Hello! I'm Yappa. Ready to yap and get stuff done!");
+        System.out.println("What are we tackling today? Let's do this!");
+        System.out.println(line);
+
+        // Exit message
+        System.out.println("Catch you later :)!");
+        System.out.println(line);
     }
 }
